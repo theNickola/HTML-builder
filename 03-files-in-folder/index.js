b2b844a8e7f97f3473a3f0d2529d908354deba30
theNickola <kurilchik.nv@gmail.com>
2023-05-04 19:17:52 +0300

    feat: implement task 3: size files

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,8 +4,11 @@ const fs = require('fs');
 try {
   fs.readdir('03-files-in-folder/secret-folder', {withFileTypes: true}, (error, files) => {
     for (const file of files) {
-      if (!file.isDirectory()) {
-        stdout.write(file.name + '\t');
+      if (file.isFile()) {
+        fs.stat('./03-files-in-folder/secret-folder/' + file.name, (err, stats) => {
+          stdout.write(file.name + '\t');
+          stdout.write(stats.size / 1024 + 'kb\n');
+        });
         console.log(file);
       }
     }
