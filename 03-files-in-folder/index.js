113fdfa27e4e423a8d700a0788979efddfd0e482
theNickola <kurilchik.nv@gmail.com>
2023-05-04 19:54:37 +0300

    feat: implement task 3: complete

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,15 +1,21 @@
 const { stdout } = process;
 const fs = require('fs');
+const path = require('path');
 
 try {
   fs.readdir('03-files-in-folder/secret-folder', {withFileTypes: true}, (error, files) => {
     for (const file of files) {
       if (file.isFile()) {
         fs.stat('./03-files-in-folder/secret-folder/' + file.name, (err, stats) => {
-          stdout.write(file.name + '\t');
+          let name = path.basename(file.name);
+          let extname = path.extname(file.name);
+          name = name.replace(extname, '');
+          extname = extname.slice(1);
+
+          stdout.write(name + ' - ');
+          stdout.write(extname + ' - ');
           stdout.write(stats.size / 1024 + 'kb\n');
         });
-        console.log(file);
       }
     }
   });
