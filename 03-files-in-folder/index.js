e1604cf2fb084e6440c4267802e6c68a40644762
theNickola <kurilchik.nv@gmail.com>
2023-05-05 17:42:14 +0300

    refactor: implement task 3: mini refactoring

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 const path = require('path');
 
 try {
-  fs.readdir('03-files-in-folder/secret-folder', {withFileTypes: true}, (error, files) => {
+  const pathDir = path.join('03-files-in-folder', 'secret-folder');
+  fs.readdir(pathDir, {withFileTypes: true}, (error, files) => {
     for (const file of files) {
       if (file.isFile()) {
-        fs.stat('./03-files-in-folder/secret-folder/' + file.name, (err, stats) => {
+        fs.stat(path.join(pathDir, file.name), (err, stats) => {
           let name = path.basename(file.name);
           let extname = path.extname(file.name);
           name = name.replace(extname, '');
