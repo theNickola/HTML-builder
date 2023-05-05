8c1620ee3e04e782f17caea2243fadd9f03021f3
theNickola <kurilchik.nv@gmail.com>
2023-05-05 15:41:32 +0300

    feat: implement task 4: create dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,8 @@
+const path = require('path');
 
+const copyDir = () => {
+  const fs = require('fs');
+  fs.mkdir(path.join('04-copy-directory','files-copy'), {recursive: true}, () => false );
+};
+
+copyDir();
