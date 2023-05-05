a88e7574565ad60f515e82896cbd54e7c8907349
theNickola <kurilchik.nv@gmail.com>
2023-05-05 22:58:48 +0300

    feat: implement task 5: create bundle and append data from CSS's

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,10 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 const pathDir = path.join('05-merge-styles', 'styles');
+const pathBundleCSS = path.join('05-merge-styles', 'project-dist', 'bundle.css');
+
 fs.readdir(pathDir, {withFileTypes: true}, (error, files) => {
+  fs.writeFile(pathBundleCSS, '', () => null);
   for (const file of files) {
-    if (path.extname(file.name).toLowerCase() === '.css') {
-      console.log(file.name);
+    if (file.isFile() && path.extname(file.name).toLowerCase() === '.css') {
+      fs.readFile(path.join(pathDir, file.name), 'utf-8', (err, data) => {
+        fs.appendFile(pathBundleCSS, data + '\n', () => null);
+      });
     }
   }
 });
