fb1f10a160fdff1a718eaa04d87205552a0c299b
theNickola <kurilchik.nv@gmail.com>
2023-05-05 17:36:43 +0300

    feat: implement task 4: recreate directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,20 +3,21 @@ const fsPromises = fs.promises;
 const path = require('path');
 
 const copyDir = (pathDir, pathNewDir) => {
-  fs.mkdir(pathNewDir, {recursive: true}, () => false );
-
-  fs.readdir(pathDir, {withFileTypes: true}, (error, files) => {
-    for (const file of files) {
-      if (file.isFile()) {
-        fsPromises.copyFile(
-          path.join(pathDir, file.name), 
-          path.join(pathNewDir, file.name));
+  fs.rm(pathNewDir, { recursive: true, force: true }, () => {
+    fs.readdir(pathDir, {withFileTypes: true}, (error, files) => {
+      fs.mkdir(pathNewDir, {recursive: true}, () => false );
+      for (const file of files) {
+        if (file.isFile()) {
+          fsPromises.copyFile(
+            path.join(pathDir, file.name), 
+            path.join(pathNewDir, file.name));
+        }
+        else if (file.isDirectory()) {
+          fs.mkdir(path.join(pathNewDir, file.name), {recursive: true}, () => false );
+          copyDir(path.join(pathDir, file.name), path.join(pathNewDir, file.name));
+        }
       }
-      else if (file.isDirectory()) {
-        fs.mkdir(path.join(pathNewDir, file.name), {recursive: true}, () => false );
-        copyDir(path.join(pathDir, file.name), path.join(pathNewDir, file.name));
-      }
-    }
+    });
   });
 };
 
