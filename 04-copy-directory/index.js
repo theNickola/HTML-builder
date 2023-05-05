cc3f3062822ede366d8471e74e87d5f99a33f792
theNickola <kurilchik.nv@gmail.com>
2023-05-05 16:21:40 +0300

    feat: implement task 4: copy files and dir without files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,23 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
 const path = require('path');
 
-const copyDir = () => {
-  const fs = require('fs');
-  fs.mkdir(path.join('04-copy-directory','files-copy'), {recursive: true}, () => false );
+const copyDir = (pathDir, pathNewDir) => {
+  fs.mkdir(pathNewDir, {recursive: true}, () => false );
+
+  fs.readdir(pathDir, {withFileTypes: true}, (error, files) => {
+    for (const file of files) {
+      if (file.isFile()) {
+        fsPromises.copyFile(
+          path.join('04-copy-directory', 'files', file.name), 
+          path.join('04-copy-directory', 'files-copy', file.name));
+      }
+      else if (file.isDirectory())
+        fs.mkdir(path.join('04-copy-directory/files-copy',file.name), {recursive: true}, () => false );
+    }
+  });
 };
 
-copyDir();
+const source = path.join('04-copy-directory', 'files');
+const destination = path.join('04-copy-directory', 'files-copy');
+copyDir(source, destination);
