f8717e266b04056dc16a5556a0b2db1a1e9ceba8
theNickola <kurilchik.nv@gmail.com>
2023-05-06 11:52:45 +0300

    feat: implement task 6: write data in index.html

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -8,19 +8,18 @@ const pathDirComponents = path.join('06-build-page', 'components');
 let newData = '';
 
 fs.mkdir(pathDir, {recursive: true}, () => {
-  fs.writeFile(pathNewHtml, '', () => {
-    fs.readFile(pathTemplateHtml, 'utf-8', (err, dataTemplate) => {
-      newData = dataTemplate.toString();
-      fs.readdir(pathDirComponents, {withFileTypes: true}, (error, files) => {
-        for (const file of files) {
-          if (file.isFile() && path.extname(file.name).toLowerCase() === '.html') {
-            fs.readFile(path.join(pathDirComponents, file.name), 'utf-8', (err, dataComponent) => {
-              const onlyNameFile = file.name.toLowerCase().replace('.html','');
-              newData = newData.replace(`{{${onlyNameFile}}}`, dataComponent);
-            });
-          }
+  fs.readFile(pathTemplateHtml, 'utf-8', (err, dataTemplate) => {
+    newData = dataTemplate.toString();
+    fs.readdir(pathDirComponents, {withFileTypes: true}, (error, files) => {
+      for (const file of files) {
+        if (file.isFile() && path.extname(file.name).toLowerCase() === '.html') {
+          fs.readFile(path.join(pathDirComponents, file.name), 'utf-8', (err, dataComponent) => {
+            const onlyNameFile = file.name.toLowerCase().replace('.html','');
+            newData = newData.replace(`{{${onlyNameFile}}}`, dataComponent);
+            fs.writeFile(pathNewHtml, newData, () => null);
+          });
         }
-      });
+      }
     });
   });
 });
