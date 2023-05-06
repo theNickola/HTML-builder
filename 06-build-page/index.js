653271d9a928d1e38be98be407d25e78d092e2e8
theNickola <kurilchik.nv@gmail.com>
2023-05-06 12:02:47 +0300

    feat: implement task 6: declaration function for collect data

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,7 +5,22 @@ const pathDir = path.join('06-build-page', 'project-dist');
 const pathNewHtml = path.join(pathDir, 'index.html');
 const pathTemplateHtml = path.join('06-build-page', 'template.html');
 const pathDirComponents = path.join('06-build-page', 'components');
+const pathDirStyles = path.join('06-build-page', 'styles');
+const pathBundleCSS = path.join(pathDir, 'styles.css');
 let newData = '';
+
+const collectStyles = (dirStyles, bundle) => {
+  fs.readdir(dirStyles, {withFileTypes: true}, (error, files) => {
+    fs.writeFile(bundle, '', () => null);
+    for (const file of files) {
+      if (file.isFile() && path.extname(file.name).toLowerCase() === '.css') {
+        fs.readFile(path.join(dirStyles, file.name), 'utf-8', (err, data) => {
+          fs.appendFile(bundle, data + '\n', () => null);
+        });
+      }
+    }
+  });
+};
 
 fs.mkdir(pathDir, {recursive: true}, () => {
   fs.readFile(pathTemplateHtml, 'utf-8', (err, dataTemplate) => {
