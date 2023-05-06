afb6ec35ad78135f9846225a6e10c50817b34389
theNickola <kurilchik.nv@gmail.com>
2023-05-06 10:11:07 +0300

    feat: implement task 6: create 'index.html' in 'project-dist'

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,5 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 const pathDir = path.join('06-build-page', 'project-dist');
+const pathNewHtml = path.join(pathDir, 'index.html');
 
-fs.mkdir(pathDir, {recursive: true}, () => false );
+fs.mkdir(pathDir, {recursive: true}, () => {
+  fs.writeFile(pathNewHtml, '', () => null);
+});
