2682d1b4418dc53cc761e3ea74d189c815a0808b
theNickola <kurilchik.nv@gmail.com>
2023-05-06 10:17:18 +0300

    feat: implement task 6: read 'template.html'

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,7 +3,12 @@ const path = require('path');
 
 const pathDir = path.join('06-build-page', 'project-dist');
 const pathNewHtml = path.join(pathDir, 'index.html');
+const pathTemplateHtml = path.join('06-build-page', 'template.html');
 
 fs.mkdir(pathDir, {recursive: true}, () => {
-  fs.writeFile(pathNewHtml, '', () => null);
+  fs.writeFile(pathNewHtml, '', () => {
+    fs.readFile(pathTemplateHtml, 'utf-8', (err, data) => {
+      if(!err) console.log(data);
+    });
+  });
 });
