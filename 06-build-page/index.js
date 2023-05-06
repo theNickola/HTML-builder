057d8ecefa87f3861b500c28c0ac60288636eb65
theNickola <kurilchik.nv@gmail.com>
2023-05-06 11:46:09 +0300

    feat: implement task 6: collect data for index.html

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,18 +5,18 @@ const pathDir = path.join('06-build-page', 'project-dist');
 const pathNewHtml = path.join(pathDir, 'index.html');
 const pathTemplateHtml = path.join('06-build-page', 'template.html');
 const pathDirComponents = path.join('06-build-page', 'components');
+let newData = '';
 
 fs.mkdir(pathDir, {recursive: true}, () => {
   fs.writeFile(pathNewHtml, '', () => {
     fs.readFile(pathTemplateHtml, 'utf-8', (err, dataTemplate) => {
-      if(!err) console.log(dataTemplate);
-      console.log('********************************************');
+      newData = dataTemplate.toString();
       fs.readdir(pathDirComponents, {withFileTypes: true}, (error, files) => {
         for (const file of files) {
           if (file.isFile() && path.extname(file.name).toLowerCase() === '.html') {
             fs.readFile(path.join(pathDirComponents, file.name), 'utf-8', (err, dataComponent) => {
-              if(!err) console.log(dataComponent);
-              console.log('********************************************');
+              const onlyNameFile = file.name.toLowerCase().replace('.html','');
+              newData = newData.replace(`{{${onlyNameFile}}}`, dataComponent);
             });
           }
         }
