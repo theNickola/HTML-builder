cff629d8a7cce6b84f81fb6af73031d50280d451
theNickola <kurilchik.nv@gmail.com>
2023-05-06 10:40:10 +0300

    feat: implement task 6: read components

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,11 +4,23 @@ const path = require('path');
 const pathDir = path.join('06-build-page', 'project-dist');
 const pathNewHtml = path.join(pathDir, 'index.html');
 const pathTemplateHtml = path.join('06-build-page', 'template.html');
+const pathDirComponents = path.join('06-build-page', 'components');
 
 fs.mkdir(pathDir, {recursive: true}, () => {
   fs.writeFile(pathNewHtml, '', () => {
-    fs.readFile(pathTemplateHtml, 'utf-8', (err, data) => {
-      if(!err) console.log(data);
+    fs.readFile(pathTemplateHtml, 'utf-8', (err, dataTemplate) => {
+      if(!err) console.log(dataTemplate);
+      console.log('********************************************');
+      fs.readdir(pathDirComponents, {withFileTypes: true}, (error, files) => {
+        for (const file of files) {
+          if (file.isFile() && path.extname(file.name).toLowerCase() === '.html') {
+            fs.readFile(path.join(pathDirComponents, file.name), 'utf-8', (err, dataComponent) => {
+              if(!err) console.log(dataComponent);
+              console.log('********************************************');
+            });
+          }
+        }
+      });
     });
   });
 });
